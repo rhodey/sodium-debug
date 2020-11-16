0fd3edbf839f1d25509b7e93957f231a6df2d06f
rhodey <rhodey@anhonesteffort.org>
2020-11-16 15:25:43 -0500

    reduce poc to only sodium-universal.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const html = require('choo/html')
-
+const sodium = require('sodium-universal')
+ 
 const TITLE = 'Example'
 module.exports = view
 
@@ -8,12 +9,11 @@ function view (state, emit) {
     emit(state.events.DOMTITLECHANGE, TITLE)
   }
 
-  function click() {
-    emit('click')
-  }
+  let rnd = Buffer.allocUnsafe(12)
+  sodium.randombytes_buf(rnd)
 
   return html`<body>
     <h2>Example</h2>
-    <button onclick=${click}>CLICK!</button>
+    <p>${rnd.toString('hex')}</p>
   </body>`
 }
--- store.js
+++ store.js
@@ -1,19 +1,6 @@
-const hypercore = require('hypercore')
-const ram = require('random-access-memory')
-
 function store(state, emitter) {
-  state.core = null
-
   emitter.on('DOMContentLoaded', function () {
-    const opts = { valueEncoding: 'utf-8', sparse: true }
-    const core = hypercore(() => ram(), opts)
-
-    core.once('error', console.error)
-    audio.once('ready', () => emitter.emit('dat:ready', core))
-  })
-
-  emitter.on('dat:ready', function (core) {
-    state.core = core
+    console.log('choo choo')
   })
 }
 
